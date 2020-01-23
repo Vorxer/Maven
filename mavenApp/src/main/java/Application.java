97fb9785b782226eb7c9a7650b814b0eeada1a90
Dinura <dinuras@gmail.com>
2020-01-23 08:11:07 +0530

    Initial commit

---
+++ mavenApp/src/main/java/Application.java
@@ -0,0 +1,7 @@
+
+public class Application {
+    public static void main(String[] args) {
+        Mathematics m = new Mathematics();
+        m.add(20.0,40.0);
+    }
+}
---
+++ mavenLibrary/src/main/java/Mathematics.java
@@ -0,0 +1,20 @@
+public class Mathematics {
+    public double add(double x,double y){
+        System.out.println("Operation performed by maven Dependancy");
+        return x+y;
+
+    }
+
+    public double multiply(double x,double y){
+
+        System.out.println("Operation performed by maven Dependancy");
+        return x*y;
+
+    }
+
+    public double divide(double x,double y){
+
+        System.out.println("Operation performed by maven Dependancy");
+        return x/y;
+    }
+}
